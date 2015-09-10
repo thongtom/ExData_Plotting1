125ff535a7da99107143262d12b0533543cdc72e
thongtom <thongtom2000@yahoo.com>
2015-09-10 17:40:36 +0800

    complete

--- plot4.R
+++ plot4.R
@@ -22,7 +22,7 @@ plot(datetime, subData$Voltage, type="l", xlab="datetime", ylab="Voltage")
 plot(datetime, subData$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
 lines(datetime, subData$Sub_metering_2, type="l", col="red")
 lines(datetime, subData$Sub_metering_3, type="l", col="blue")
-legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),cex=0.7, y.intersp=0.5, xjust=0, yjust=0, bty="n", lty=1, lwd=1, col=c("black", "red", "blue"))
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty="n", lty=1, lwd=1, col=c("black", "red", "blue"))
 
 plot(datetime, subData$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 
